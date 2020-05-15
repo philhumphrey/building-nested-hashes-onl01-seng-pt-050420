3f5203e94f2012fc50bbdbea94bfe866a90ede06
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 22:37:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
        age: "53"},
       :matriarch => {name: "Lady Montague", 
       age: "54"},
-      :hero => {
+      :hero => {name: "Romeo", age: "15", status: "alive"
         #your key/value pairs here
         },
       :hero_friends => []
