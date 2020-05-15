fd02496661c95a44322a69832c192e4b3ecc53ad
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 22:53:45 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,12 +7,6 @@ def fourth_challenge
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
-        
-        
-        
-        
-        
-        #your hashes here!
       ]
    }, 
    :capulet => {
@@ -20,7 +14,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        
+        
+        
+        
+        
       ]
    }
 }
