cbf0c97a4dc2b637a5146c958932751bf617e7ca
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 22:42:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,9 +10,8 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {name: "Lord Capulet", age: "50"
         
-        #your key/value pairs here
         },
       :matriarch => {
         #your key/value pairs here
