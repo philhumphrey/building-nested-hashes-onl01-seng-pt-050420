59195442d2cdc3a12280f9db66ac4204c0393abd
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 22:19:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,11 +7,11 @@ def second_challenge
       :hero_friends => [] 
       }, 
     :capulet => {
-      
-      
-      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }
-    
   }
 
   
